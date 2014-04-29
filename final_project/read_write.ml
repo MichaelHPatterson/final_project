08bcce24a418f5b4719327e525d416ff7de6b584
Michael Patterson <michaelpatterson@college.harvard.edu>
2014-04-28 23:23:05 -0400

    read and write abstracted, almost finished

--- final_project/read_write.ml
+++ final_project/read_write.ml
@@ -13,8 +13,11 @@ sig
 
   val string_of_val : value -> string
 
-  val big_test_matrix : value array array
+  val val_of_string : string -> value
 
+  val zero_value : value
+
+  val big_test_matrix : value array array
 end
 
 (* Signature for writing to file. Note that the only exposed function is actually
@@ -32,7 +35,6 @@ sig
 
   (* runs tests for writing *)
   val run_tests : unit -> unit
-
 end
 
 
@@ -48,20 +50,20 @@ sig
 
   (* types for the matrix implementation *)
   type mat_value
-  type vec
-  type mat
+  type vec = mat_value array
+  type mat = vec array
 
   (* reads a .txt file of a specific invariant, returning the ranking matrix *)
   val process_file : string -> mat
-
-  (* runs tests for writing *)
-  (* val run_tests : unit -> unit *)
-
 end
  
 module IntMatrix : MATRIX =
 struct
   type value = int
+
+  let zero_value = 0
+
+  let val_of_string = int_of_string
 
   let string_of_val = string_of_int
 
@@ -72,27 +74,18 @@ module StringMatrix : MATRIX =
 struct
   type value = string
 
+  let zero_value = ""
+
+  let val_of_string x = x
+
   let string_of_val x = x
 
   let big_test_matrix = [| [| "Po"; "Doc"; "Pinky"; "Miles" |]; [| "A"; "B"; "C"; "D" |]; [| "1"; "2"; "3"; "4" |];
 			   [| "a"; "b"; "c"; "d" |] |]
 end
 
-
-(* Module that writes matrices to a text file. Note that, in its current implementation,
- * this can only be used with int matrices. However, it could, and should, be made
- * abstract. That will come soon. Also, forgive the poor style; lots of lines are
- * longer than 80 chars *)
-
-(* TODO for Write:
-   -- Make this abstract. Use a functor for "value" and corresponding functions.
-   -- Prepare for user input to be implemented in the near future.
-   -- Prepare for labelling of matrix cols/rows with owners, elements
-   -- Fix style
-   -- Run more extensive tests
-*)
-
-module IntWrite(M: MATRIX) : (WRITE with type value = M.value) =
+(* Writes matrices of some type given by M to a text file. *)
+module Write(M: MATRIX) : (WRITE with type value = M.value) =
 struct
   type value = M.value
   type vec = value array
@@ -103,7 +96,7 @@ struct
     Array.fold matrix ~init:init ~f:(fun acc x -> Array.fold x ~init:acc ~f:f)
 
   (* general map function for matrices; probably should go in matrix.ml *)
-  let matrix_map matrix f =
+  let matrix_map (matrix : 'a array array) ~(f : 'a -> 'b) : 'b array array =
     let len = Array.length matrix in
     let result = Array.create ~len:len (Array.create ~len:len (f (matrix.(0)).(0))) in
     for i = 0 to len - 1 do
@@ -131,7 +124,7 @@ struct
 
   (* actually writes to file by converting to string matrix, formatting, and writing *)
   let mat_to_file (matrix : mat) (filename : string) : unit =
-    let string_matrix = matrix_map matrix M.string_of_val in
+    let string_matrix = matrix_map matrix ~f:M.string_of_val in
     let max_len = matrix_fold string_matrix ~f:(fun acc x -> max (String.length x) acc) ~init: 0 in
     let to_strings = string_mat_to_strings string_matrix (max_len + 1) in
     Out_channel.write_lines filename to_strings
@@ -148,7 +141,7 @@ struct
 
   let test_mat_to_file () =
     let my_mat = M.big_test_matrix in
-    mat_to_file my_mat "joe.txt"
+    mat_to_file my_mat "int_write_test.txt"
 
   let run_tests () =
     test_row ();
@@ -157,37 +150,38 @@ struct
     ()
 end
 
-module IntMatrixModule = IntWrite(IntMatrix);;
-
+(* tests for an int matrix *)
+module IntMatrixModule = Write(IntMatrix);;
 IntMatrixModule.run_tests();;
 
+(* tests for a string matrix *)
+module StringMatrixModule = Write(StringMatrix);;
+StringMatrixModule.mat_to_file (StringMatrix.big_test_matrix) "string_write_test.txt";;
 
-module StringMatrixModule = IntWrite(StringMatrix);;
-
-StringMatrixModule.run_tests();;
-
-(* Signature for reading a matrix. Note that, like IntWrite, the main issue 
- * with this module is its lack of abstraction. This will come shortly. Also,
- * style will also be fixed in the future. *)
-
-(* On abstraction: Note that, because this module utilizes multiple data
- * structures, i.e. two dicts and a matrix, that abstractions could be made 
- * to both the type of dict used and the type of ranking used *)
-
-module IntRead(D: DICT) : (READ with type key = D.key
-  with type value = D.value with type dict = D.dict) = 
+(* Signature for reading a file and building a matrix, with dicts for indexing
+ * the rows and columns. Requires a matrix functor, which defines the type of 
+ * the matrix. Also requires a dict functor, which defines the dict that is 
+ * stored. *)
+module Read (M: MATRIX) (D: DICT) : (READ with type key = D.key
+  with type value = D.value with type dict = D.dict with type mat_value = M.value) = 
 struct
 
   type key = D.key
   type value = D.value
   type dict = D.dict
 
-  type mat_value = int
-  type vec = int array
+  type mat_value = M.value
+  type vec = mat_value array
   type mat = vec array
 
   (* instantiates a square matrix of len dimensions *)
-  let rank_matrix (len : int) = Array.create ~len:len (Array.create ~len:len 0)
+  let rank_matrix (len : int) = Array.make_matrix len len M.zero_value
+  
+  (* This fucks things up -- all of the columns become uniform. I think
+   * this is because the m.(x).(y) notation doesn't work for normal
+   * two-dimensional arrays. However, it works for the make_matrix two-
+   * dimensional arrays:
+   * Array.create ~len:len (Array.create ~len:len M.zero_value) *)
   
   (* a ref for storing owners and their indices *)
   let owner_dict = ref (D.empty)
@@ -199,21 +193,27 @@ struct
   (* a ref for storing the current max element index *)
   let elt_index = ref 0
 
-  (* converts the element-rank line into a (string, int) tuple by splitting at
+  (* converts the element-rank line into a (string, mat_value) tuple by splitting at
    * the colon *)
   let process_elt (line : string) =
     let (a, b) = String.rsplit2_exn line ':' in
-    (String.strip a, int_of_string (String.strip b))
+    (String.strip a, M.val_of_string (String.strip b))
     
-  let process_file (filename : string) =
+  let process_file (filename : string) : mat =
     let file_lines = In_channel.read_lines filename in
+
     (* checks if the line is for an owner or element by looking for a colon in
      * the line; this is an invariant of the .txt file *)
-    let elt_check (line : string) = String.contains line ':' in
+    let elt_check (line : string) : bool = String.contains line ':' in
     let owner_num = List.fold_right file_lines 
       ~f:(fun x acc -> if not (elt_check x) then acc + 1 else acc) ~init:0 in
     let return_matrix = rank_matrix owner_num in
+
+    let update_matrix (matrix : mat) (row : int) (col : int) (new_val : mat_value) : unit =
+      matrix.(row).(col) <- new_val in
+
     let add_to_dict (subject : string) : unit =
+      if ((String.strip subject) = "") then () else
       if not (elt_check subject) then (
 	if D.member (!owner_dict) (D.key_of_string (String.strip subject)) then
 	  failwith "owner already in dict"
@@ -224,18 +224,18 @@ struct
 	match D.lookup (!elt_dict) (D.key_of_string a) with
 	| None -> (
 	   elt_dict := D.insert (!elt_dict) (D.key_of_string a) (D.val_of_int (!elt_index));
-	   (return_matrix.(!owner_index - 1)).(!elt_index) <- b;
+	   update_matrix return_matrix (!owner_index - 1) (!elt_index) b;
 	   elt_index := (!elt_index) + 1)
 	| Some x ->
-	   (return_matrix.(!owner_index - 1)).(int_of_string (D.string_of_value x))
-	   <- b) in
+	   update_matrix return_matrix (!owner_index - 1) (int_of_string (D.string_of_value x)) b) in
     List.iter file_lines ~f:add_to_dict;
     return_matrix
-(*
-  let test_read () = mat_to_file (process_file "jane.txt") "test_mat.txt"
-
-  let run_tests () =
-    test_read ();
-    ()
- *)
 end
+
+(* Tests for Reading and then Writing *) 
+module IntDrugRead = Read (IntMatrix) (Make(StringIntDictArg));;
+module IntDrugWrite = Write (IntMatrix);;
+IntDrugWrite.mat_to_file (IntDrugRead.process_file "test_input.txt") "test_output.txt";;
+ 
+(* This works by itself, but I need to make some dict modifications *)
+(* let my_matrix = IntDrugRead.process_file "jane.txt";; *)
