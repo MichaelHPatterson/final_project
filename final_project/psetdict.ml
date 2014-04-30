d649bc27f428efed81eb3ced27abb18d51c81b6a
Michael Patterson <michaelpatterson@college.harvard.edu>
2014-04-30 05:25:16 -0400

    tentative finish of insert

--- final_project/psetdict.ml
+++ final_project/psetdict.ml
@@ -94,6 +94,8 @@ struct
   type key = string
   type value = int
   let rec compare x y = 
+    (* Less means that x comes alphabetically before y; Greater means that it
+     * comes after *)
     match String.compare x y with
     | 0 -> Equal
     | -1 -> Less
@@ -279,7 +281,97 @@ struct
     ()
 
 end
-  
+
+module BSTDict(D:DICT_ARG) : (DICT with type key = D.key
+  with type value = D.value) =
+struct
+  type key = D.key
+  type value = D.value
+
+  (* A dictionary entry is a (key, value) pair. *)
+  type pair = key * value
+
+  (* Type definition for dictionary as a BST *)
+  type dict = 
+    | Leaf of int
+    | Two of int * dict * pair * dict
+
+  let k_compare p1 p2 =
+    let (x, _) = p1 in
+    let (y, _) = p2 in
+    D.compare x y
+
+  let empty : dict = Leaf 0
+
+  let rec insert (d : dict) (p : pair) : dict =
+    match d with
+    | Leaf _ -> Two(1, Leaf 0, p, Leaf 0)
+    | Two (_, left, p1, right) -> 
+       (match k_compare p p1 with
+	| Equal -> failwith "already in dict"
+	| Less -> 
+	   let new_left = insert left p in
+	   balance Two(get_height new_left, new_left, p1, right)
+	| Greater -> 
+	   let new_right = insert right p in
+	   balance Two(get_height left new_right, left, p1, new_right))
+
+  let rec get_height (d1 : dict) (d2 : dict) : int = 
+    match (d1, d2) with
+    | (Leaf _, Leaf _) -> 1
+    | (x, y) -> (max (get_height x) (get_height y)) + 1
+
+  let extract_height (d : dict) : int = 
+    let Two (x, _, _, _) = d in x			 
+
+  let balance (d : dict) : dict =
+    let left_left (input : dict) : dict =
+      let Two (my_height, l_dict, my_pair, r_dict) = input in
+      let Two (_, l_ldict, l_pair, l_rdict) = l_dict in
+      Two(my_height-1, l_ldict, l_pair, Two(my_height-2, l_rdict, my_pair, 
+        r_dict)) in
+    
+    let left_right (input : dict) : dict =
+      let Two (my_height, l_dict, my_pair, r_dict) = input in
+      let Two (_, l_ldict, l_pair, l_rdict) = l_dict in
+      let Two (_, l_r_ldict, l_rpair, l_r_rdict) = l_rdict in
+      Two(my_height, Two(my_height-1, Two(my_height-2, l_ldict, l_pair,
+        l_r_ldict), l_rpair, l_r_rdict), my_pair, r_dict) in
+
+    let right_right (input : dict) : dict =
+      let Two (my_height, l_dict, my_pair, r_dict) = input in
+      let Two (_, r_ldict, r_pair, r_rdict) = r_dict in
+      Two(my_height-1, Two(my_height-2, l_dict, my_pair, r_ldict), r_pair,
+        r_rdict) in
+
+    let right_left (input : dict) : dict =
+      let Two (my_height, l_dict, my_pair, r_dict) = input in
+      let Two (_, r_ldict, r_pair, r_rdict) = r_dict in
+      let Two (_, r_l_ldict, r_lpair, r_l_rdict) = r_ldict in
+      Two(my_height, l_dict, my_pair, Two(my_height-1, r_l_ldict, r_lpair,
+        Two(my_height-2, r_l_rdict, r_pair, r_rpair))) in
+
+    let balance_factor (my_dict : dict) : dict =
+      let Two (_, left, _, right) = my_dict in
+      (extract_height left) - (extract_height right) in
+    let Two (dict_height, dict_left, dict_pair, dict_right) = d in
+    match balance_factor d with
+    | 2 -> (
+       let bal_left = balance_factor dict_left in
+       if bal_left = -1 then left_left(left_right d)
+       else (if (bal_left = 0 || bal_left = 1) then left_left d
+	     else failwith "invalid balance_factor"))
+    | -2 -> (
+      let bal_right = balance_factor dict_right in
+      if bal_right = 1 then right_right(right_left d)
+      else (if (bal_right = 0 || bal_right = -1) then right_right d
+	    else failwith "invalid balance_factor"))
+    | x -> if (x < 3 && x > -3) then d else failwith "balance_factor out of
+						      bounds"
+    
+    
+
+
 module Make (D:DICT_ARG) : (DICT with type key = D.key
   with type value = D.value) =
   AssocListDict(D)
