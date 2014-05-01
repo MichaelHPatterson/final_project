0a2fe955a265fce8a9ddaf5f47d87ba5dc93e959
Michael Patterson <michaelpatterson@college.harvard.edu>
2014-04-30 21:05:18 -0400

    moved changes over to psetdict.ml

--- final_project/psetdict.ml
+++ final_project/psetdict.ml
@@ -298,31 +298,57 @@ struct
 
   (* Type definition for dictionary as a BST *)
   type dict = 
-    | Leaf of int
+    | Leaf
     | Two of int * dict * pair * dict
 
+  let empty : dict = Leaf
+
+  let rec fold (f : key -> value -> 'a -> 'a) (init : 'a) (d : dict) : 'a =
+    match d with
+    | Leaf -> init
+    | Two (_, left, (k, v), right) -> fold f (fold f (f k v init) right) left
+  
+  let key_compare x y =
+    if x < y then Less else if x > y then Greater else Equal
+
+  let rec lookup (d : dict) (k : key) : value option =
+    match d with
+    | Leaf -> None
+    | Two (_, left, (k1, v1), right) ->
+       (match key_compare k k1 with
+	| Equal -> Some v1
+	| Less -> lookup left k
+	| Greater -> lookup right k)
+  
   let k_compare p1 p2 =
     let (x, _) = p1 in
     let (y, _) = p2 in
     if x < y then Less else if x > y then Greater else Equal
 
-  let empty : dict = Leaf 0
+  let member d k = (lookup d k) <> None
 
   let rec get_height (d1 : dict) (d2 : dict) : int = 
     match (d1, d2) with
-    | (Leaf _, Leaf _) -> 1
-    | (Leaf _, Two (_, left, _, right)) -> (get_height left right) + 1
-    | (Two (_, left, _, right), Leaf _) -> (get_height left right) + 1
+    | (Leaf, Leaf) -> 1
+    | (Leaf, Two (_, left, _, right)) -> (get_height left right) + 1
+    | (Two (_, left, _, right), Leaf) -> (get_height left right) + 1
     | (Two (_, left1, _, right1), Two (_, left2, _, right2)) ->
        (max (get_height left1 right1) (get_height left2 right2)) + 1
 
   let extract_height (d : dict) : int = 
-    let Two (x, _, _, _) = d in x			 
+    match d with
+    | Leaf -> 0
+    | Two (x, _, _, _) -> x
 
   let is_leaf (d : dict) : bool =
     match d with
-    | Leaf _ -> true
+    | Leaf -> true
     | _ -> false
+
+  let balance_factor (my_dict : dict) : int =
+    if is_leaf my_dict then failwith "got Leaf in balance_factor" else
+      let Two (_, left, _, right) = my_dict in
+      (extract_height left) - (extract_height right)
 
   let balance (d : dict) : dict =
     let left_left (input : dict) : dict =
@@ -355,10 +381,6 @@ struct
       Two(my_height, l_dict, my_pair, Two(my_height-1, r_l_ldict, r_lpair,
         Two(my_height-2, r_l_rdict, r_pair, r_rdict))) in
 
-    let balance_factor (my_dict : dict) : int =
-      if is_leaf my_dict then failwith "got Leaf in balance_factor" else
-      let Two (_, left, _, right) = my_dict in
-      (extract_height left) - (extract_height right) in
     if is_leaf d then d else (
     let Two (dict_height, dict_left, dict_pair, dict_right) = d in
     match balance_factor d with
@@ -377,7 +399,7 @@ struct
     
   let rec insert (d : dict) (p : pair) : dict =
     match d with
-    | Leaf _ -> Two(1, Leaf 0, p, Leaf 0)
+    | Leaf -> Two(1, Leaf, p, Leaf)
     | Two (_, left, p1, right) -> 
        (match k_compare p p1 with
 	| Equal -> failwith "already in dict"
@@ -387,6 +409,46 @@ struct
 	| Greater -> (
 	   let new_right = insert right p in
 	   balance (Two(get_height left new_right, left, p1, new_right))))
+
+  (* In the interest of time, this remove function has been coded inefficiently.
+   * Our program will not require the removal of entries from a dictionary, so
+   * this is meant simply to appease the requirements of the signature. *)
+  let rec remove (d : dict) (my_key : key) : dict =
+    fold (fun k v acc -> if (k_compare my_key k = Equal) then acc
+			 else insert acc k v) empty d
+
+  (* Also coded in the interest of time, because this has no use in our program
+   * and is only coded for appeasing the sig *)
+  let choose (d : dict) : (key * value * dict) option =
+    match d with
+    | Leaf -> None
+    | Two (_, _, (k, v), _) -> Some (k, v, remove d k)
+
+  let rec is_balanced (d : dict) : bool = 
+    match d with
+    | Leaf -> true
+    | Two (_, left, _, right) -> let factor = balance_factor d in
+        (factor < 2 && factor > -2) && (is_balanced left) && (is_balanced right)
+
+  let run_tests () : unit =
+    let rand : int -> int = Random.self_init () ; Random.int in
+    let rec list_gen (num : int) (lst : int list) : int list =
+      if num <= 0 then lst
+      else (let rand_num = rand 250 in
+	   if not (List.mem lst rand_num) 
+	   then list_gen (num - 1) (rand_num :: lst)
+	   else list_gen num lst) in
+    let add_list_to (lst : int list) : dict =
+      List.fold_right lst ~f:(fun x acc -> insert acc (x, "")) ~init:empty in
+    let print_list (lst : int list) : unit =
+      List.iter lst (
+        fun x -> Out_channel.output_string stdout (string_of_int x ^ "\n");
+		 Out_channel.flush stdout) in
+    for i = 0 to 50 do
+      let rand_list = list_gen 50 [] in
+      if is_balanced (add_list_to (rand_list)) then ()
+      else print_list rand_list
+    done
 end
 
 module Make (D:DICT_ARG) : (DICT with type key = D.key
