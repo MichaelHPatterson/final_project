590acdb34550ecb09d233e5e21e0945767ce4877
Madhusudan Vijay <mvijay@college.harvard.edu>
2014-05-03 17:34:57 -0400

    minor changes

--- final_project/main.ml
+++ final_project/main.ml
@@ -34,46 +34,43 @@ module FloatWrite = Write(FloatMatrixArg)(MakeDict);;
  *)
 let run_algorithms (input : string) (output : string) : unit =
   try (
-  let time1 = Unix.gettimeofday () in
-  let (input_mat, owner_dict, elt_dict) = FloatRead.process_file input in
-  let formatted_input = 
-    FloatWrite.data_formatted (input_mat, owner_dict, elt_dict) in
-  let time2 = Unix.gettimeofday () in
-  let pagerank_mat = Pagerank.pageranks(input_mat) in
-  let time3 = Unix.gettimeofday () in
-  let pagerank_formatted = 
-    FloatWrite.data_formatted (pagerank_mat, owner_dict, elt_dict) in
-  let time4 = Unix.gettimeofday () in
-  let cost_convert = Pagerank.cost_matrix(pagerank_mat) in
-  let time5 = Unix.gettimeofday () in
-  let cost_formatted = 
-    FloatWrite.data_formatted (cost_convert, owner_dict, elt_dict) in
-  let time6 = Unix.gettimeofday () in
-  let hungarian_results = Hungarian.hungarian(cost_convert) in
-  let time7 = Unix.gettimeofday () in
-  let hungarian_formatted = Hungarian.format_hungarian hungarian_results
-    owner_dict elt_dict in
-  let time8 = Unix.gettimeofday () in
-
-  let lists_append (lst : string list list) : string list =
-    List.fold_right lst ~f:(fun x acc -> x @ acc) ~init:[] in
-  let formatted_output = lists_append [["Input Rankings:\n"]; 
-    formatted_input; ["Pagerank Rankings:\n"]; pagerank_formatted;
-    ["Cost Matrix\n"]; cost_formatted; ["Hungarian Algorithm Results\n"];
-    hungarian_formatted] in
-  let time9 = Unix.gettimeofday () in
-  Out_channel.write_lines output formatted_output;
-  let time10 = Unix.gettimeofday () in
-  Printf.printf "Time for reading and formatting input: %f\n" (time2 -. time1);
-  Printf.printf "Time for computing M*M^T and matrix exponential: %f\n" (time3 -. time2);
-  Printf.printf "Time for formatting PageRanks for printing: %f\n" (time4 -. time3);
-  Printf.printf "Time for converting to a cost matrix: %f\n" (time5 -. time4);
-  Printf.printf "Time for formatting the cost matrix for printing: %f\n" (time6 -. time5);
-  Printf.printf "Time for applying the Hungarian algorithm: %f\n" (time7 -. time6);
-  Printf.printf "Time for formatting the Hungarian algorithm results for printing: %f\n" (time8 -. time7);
-  Printf.printf "Time for combining all output for printing: %f\n" (time9 -. time8);
-  Printf.printf "Time for printing to files: %f\n" (time10 -. time9);
-  flush_all ())
+    let time1 = Unix.gettimeofday () in
+    let (input_mat, owner_dict, elt_dict) = FloatRead.process_file input in
+    let formatted_input = 
+      FloatWrite.data_formatted (input_mat, owner_dict, elt_dict) in
+    let time2 = Unix.gettimeofday () in
+    let pagerank_mat = Pagerank.pageranks(input_mat) in
+    let time3 = Unix.gettimeofday () in
+    let pagerank_formatted = 
+      FloatWrite.data_formatted (pagerank_mat, owner_dict, elt_dict) in
+    let time4 = Unix.gettimeofday () in
+    let cost_convert = Pagerank.cost_matrix(pagerank_mat) in
+    let time5 = Unix.gettimeofday () in
+    let cost_formatted = 
+      FloatWrite.data_formatted (cost_convert, owner_dict, elt_dict) in
+    let time6 = Unix.gettimeofday () in
+    let hungarian_results = Hungarian.hungarian(cost_convert) in
+    let time7 = Unix.gettimeofday () in
+    let hungarian_formatted = Hungarian.format_hungarian hungarian_results
+							 owner_dict elt_dict in
+    let time8 = Unix.gettimeofday () in
+    let formatted_output = List.concat [["Input Rankings:\n"]; 
+      formatted_input; ["Pagerank Rankings:\n"]; pagerank_formatted;
+      ["Cost Matrix\n"]; cost_formatted; ["Hungarian Algorithm Results\n"];
+      hungarian_formatted] in
+    let time9 = Unix.gettimeofday () in
+    Out_channel.write_lines output formatted_output;
+    let time10 = Unix.gettimeofday () in
+    Printf.printf "Time for reading and formatting input: %f\n" (time2 -. time1);
+    Printf.printf "Time for computing M*M^T and matrix exponential: %f\n" (time3 -. time2);
+    Printf.printf "Time for formatting PageRanks for printing: %f\n" (time4 -. time3);
+    Printf.printf "Time for converting to a cost matrix: %f\n" (time5 -. time4);
+    Printf.printf "Time for formatting the cost matrix for printing: %f\n" (time6 -. time5);
+    Printf.printf "Time for applying the Hungarian algorithm: %f\n" (time7 -. time6);
+    Printf.printf "Time for formatting the Hungarian algorithm results for printing: %f\n" (time8 -. time7);
+    Printf.printf "Time for combining all output for printing: %f\n" (time9 -. time8);
+    Printf.printf "Time for printing to files: %f\n" (time10 -. time9);
+    flush_all ())
   with (Sys_error error) ->
     if error = input ^ ": No such file or directory" then
       Printf.printf "The input file does not exist!\n"
